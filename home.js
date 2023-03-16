2ec0a87d47f90c6dc654e53c9be6f291cc12af49
Laetitia <laetitia.mdlb@gmail.com>
2023-03-16 16:15:05 +0100

    creation home page

---
+++ home.js
@@ -0,0 +1,23 @@
+const categoriesContainer = document.querySelector(".food-categories")
+
+const foodCategories = [
+    {name: "Italien 🍕", backgroundColor: '#FEED58' },
+    {name: "Vegan 🍃", backgroundColor: '#7DED89' },
+    {name: "Sushi 🍣", backgroundColor: '#FF9595' },
+    {name: "Burger 🍔", backgroundColor: '#FFB94F' },
+    {name: "Brunch 🥞", backgroundColor: '#9BCFFF' },
+    {name: "Thaï 🍛", backgroundColor: '#CD995C' },
+]
+
+for (let i = 0; i < foodCategories.length; i++) {
+    console.log("salut")
+    const buttonCategories = document.createElement("div");
+    const titleCategories = document.createElement("p");
+    buttonCategories.classList.add("btn-categories");
+    titleCategories.innerHTML = foodCategories[i].name;
+    buttonCategories.style.backgroundColor = foodCategories[i].backgroundColor;
+    categoriesContainer.appendChild(buttonCategories);
+    buttonCategories.appendChild(titleCategories);
+
+}
+
