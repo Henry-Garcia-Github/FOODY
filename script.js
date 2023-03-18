c66cce53d23e0614e975ce6ed91105d18f197e19
Henry DEMAISON <henry31150@hotmail.fr>
2023-03-18 15:13:30 +0100

    script.js popup added

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ if (mediaQueryMin.matches) {
 } else {
   welcomeLinkRedirect.href = './home.html';
 }
+
+function popUp(string){
+const popUp = document.createElement('div');
+const popUpText = document.createElement('p');
+popUp.classList.add('popUp');
+popUpText.innerHTML = string;
+popUp.appendChild(popUpText);
+document.body.appendChild(popUp);
+}
+
+//const passwordLink = document.querySelector('.mdp');
+//passwordLink.addEventListener('onclick',function(){
+//  alert('test')
+//});
+
+popUp(test)
