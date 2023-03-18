1f484cbcb92f9a2bc8801dc9783841f0943ccfc8
Raphaël Martel <contact@raphaelmartel.com>
2023-03-18 15:42:17 +0100

    Update popUp

--- popup.js
+++ popup.js
@@ -1,14 +1,11 @@
-// function popUp(string) {
-//   const popUp = document.createElement('div');
-//   const popUpText = document.createElement('p');
-//   popUp.classList.add('popUp');
-//   popUpText.innerHTML = string;
-//   popUp.appendChild(popUpText);
-//   document.body.appendChild(popUp);
-// }
-
 const popUpBtn = document.querySelector('.popup-btn');
 
 popUpBtn.addEventListener('click', () => {
-  alert('Hello World');
+  const popUp = document.createElement('div');
+  const popUpText = document.createElement('p');
+  popUp.classList.add('popUp');
+  popUpText.innerHTML = 'Hello';
+  popUp.appendChild(popUpText);
+  document.body.appendChild(popUp);
+  
 });
