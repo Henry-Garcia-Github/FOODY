bb6b712a13cf40b8d8c24703796dd052ba66c9a3
Raphaël Martel <contact@raphaelmartel.com>
2023-03-21 16:26:04 +0100

    Add event prevent default

--- login.js
+++ login.js
@@ -1,15 +1,15 @@
-const userLogin = "admin@foody.com";
-const userPassword = "admin";
-const btnLogin = document.querySelector(".btn-full");
-const inputLogin = document.querySelector("#name");
-const inputPassword = document.querySelector("#password");
+const userLogin = 'admin@foody.com';
+const userPassword = 'admin';
+const btnLogin = document.querySelector('.btn-full');
+const inputLogin = document.querySelector('#name');
+const inputPassword = document.querySelector('#password');
 
-btnLogin.addEventListener('click', function(event){
-if((document.getElementById('name').value === userLogin) && (document.getElementById('password').value === userPassword)) {
-    document.location.href="home.html"
-} else {
-    event.preventDefault();
+btnLogin.addEventListener('click', function (event) {
+  event.preventDefault();
+  if (document.getElementById('name').value === userLogin && document.getElementById('password').value === userPassword) {
+    document.location.href = './home.html';
+  } else {
     inputLogin.style.border = '2px solid red';
     inputPassword.style.border = '2px solid red';
   }
-})
+});
