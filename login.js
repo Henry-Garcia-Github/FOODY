a7d1f45dbf6486f51559e98cbc9e0eee1aa28354
Henry DEMAISON <henry31150@hotmail.fr>
2023-03-21 12:06:48 +0100

    js modified for error password

--- login.js
+++ login.js
@@ -1,8 +1,15 @@
 const userLogin = "admin@foody.com";
 const userPassword = "admin";
-const btnLogin = document.querySelector(".btn btn-full");
+const btnLogin = document.querySelector(".btn-full");
+const inputLogin = document.querySelector("#name");
+const inputPassword = document.querySelector("#password");
 
-btnLogin.addEventListener('click', function(){
-if((document.getElementById('name') == userLogin) && (document.getElementById('password') == userPassword)) {
+btnLogin.addEventListener('click', function(event){
+if((document.getElementById('name').value === userLogin) && (document.getElementById('password').value === userPassword)) {
     document.location.href="home.html"
-}})
+} else {
+    event.preventDefault();
+    inputLogin.style.border = '2px solid red';
+    inputPassword.style.border = '2px solid red';
+  }
+})
